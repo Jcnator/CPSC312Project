efadf116f2e3183db789891560bd24482f0b6f54
Jcnator <56492289+Jcnator@users.noreply.github.com>
2019-10-13 18:24:19 -0700

    Update Roller.hs

--- Roller.hs
+++ Roller.hs
@@ -1,16 +1,16 @@
 module Roller where
 
 import System.Random
+import Data.List
 
 data Roll  = Total  Int
-            | Dice [Int]
+            | Dice  [Int]
 
 -- rollSum :: Int -> [IO Int] -> Int
 -- rollSum total rolls = do
 --   n <- rolls !! 1
 --   drop 1 rolls
 --   rollSum (total+n) rolls
-
 
 
 listRoller :: Int -> Int ->[Int] -> IO [Int]
@@ -22,7 +22,26 @@ listRoller a b list =
       n <- getStdRandom (randomR (1,b))
       listRoller (a-1) b (n : list)
 
+listSum :: [Int]->Int
+listSum list =
+  foldr (+)  0 list
 
+
+listKstH :: [Int]-> Int ->Int
+listKstH list  k =
+  (reverse (sort list)) !! (k-1)
+
+listKstL :: [Int]-> Int ->Int
+listKstL list  k =
+  (sort list) !! (k-1)
+
+listKeepHklist :: [Int] -> Int -> [Int]
+listKeepHlist list k =
+  fst (splitAt k (reverse (sort list)))
+
+listKeepLklist :: [Int] -> Int -> [Int]
+listKeepLlist list k =
+    fst (splitAt k (sort list))
 
 roller :: Int -> Int -> Int ->  IO Int
 roller a b total  =
@@ -35,14 +54,3 @@ roller a b total  =
       -- n : (Dice roll)
       print n
       roller (a-1) b (n + total)
-
-
-
-
--- roller a b =
---       -- g <- getStdGen
---       -- n <- fst randomR (1, b) g
---   let n = 1
---   return 0
-      --print n
-      --roller (a-1) b
