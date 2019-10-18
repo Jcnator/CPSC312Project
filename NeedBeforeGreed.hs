cd566f5275c8847d306f142342dd64ed6bde6298
Jcnator <56492289+Jcnator@users.noreply.github.com>
2019-10-17 17:31:18 -0700

    Update NeedBeforeGreed.hs

--- NeedBeforeGreed.hs
+++ NeedBeforeGreed.hs
@@ -1,56 +1,74 @@
-module NeedBeforeGreed where
+module PlayNeedBeforeGreed where
 
-  import Roller
-  import Data.List
 
-  -- Types of roll for Need before Greed.
-  -- Choosing Pass skips the game
-  -- Greed and Need are sorted on highest result of a roll1d100
-  -- Need rolls take priority
-  data RollType = Pass | Greed | Need  deriving (Eq, Show)
+import NeedBeforeGreed
+import Roller
+import Printer
 
-  -- A NB4G has 3 fields, the name of the roller, the type of roll and the result
-  data NB4G = NB4G { name     :: String
-                    ,rolltype :: RollType
-                    ,result   :: Int
-                    } deriving (Show)
+import Data.List
+import System.IO
+import Text.Read   (readMaybe)
+import Data.Maybe   (fromJust)
+import Control.Monad
 
-  -- takes a list of NB4G and sorts it as follows:
-  -- Filters out any Pass
-  -- maps roll1d100 to the Greed rolls and sorts on the result
-  -- maps roll1d100 to the Need rolls and sorts on the result
-  -- appends Greed ++ Need so that the higest Need roll is at the end of the list
-  rollNB4G :: [NB4G]->[NB4G]
-  rollNB4G listofrollers =
-    sortOn (result) (map (roll1d100) ((filter (takeGreed) listofrollers)))
-    ++
-    sortOn (result) (map (roll1d100) ((filter (takeNeed) listofrollers)))
 
-    -- rollNB4G [(NB4G "Pass1" Pass 0), (NB4G "Greed1" Greed 0), (NB4G "Greed2" Greed 0), (NB4G "Need1" Need 0), (NB4G "Need2" Need 0), (NB4G "Greed3" Greed 0)]
+-- Starts a Need Before greed.
+-- Lottery game to decide who gets to keep an item.
+-- Players will either Pass meaning they don't play
+-- Select Greed, meaining that they will be going against other Greed Rolls
+-- Or Select Need if they really need the item. Need always wins over Greed
+-- Function will be called with an empty list at the Start
+-- The function will ask for the name of the player and their roll type and
+-- recursively add them to a new instance of the game which is represented by
+-- a [NB4G].
+playNeedBeforeGreed :: [NB4G] -> IO ()
+playNeedBeforeGreed state = do
+  putStrLn "Name? (Finish with '0')"
+  line <- getLine
+  if line == "0"
+    then do
+      if state /= [] -- There is at least one player
+        then do
+          -- a new state resultState is created from the rollResult
+          -- function then the printNB4G is mapped to print out the results
+          -- the winner is found and the game delcares who won
+          let resultState = rollResult state
+          mapM (printNB4G) resultState
+          let winner = winnerNB4G resultState
+          let winner_name = name winner
+          putStrLn ("Winner is: " ++ winner_name ++ "!")
+      else do
+        -- Game ends without any players so no rolls are made
+        putStrLn "Need Before Greed has ended"
+    else do
+      putStrLn "Pass (1), Greed (2) or Need (3)?"
+      line2 <- getLine
+      if line2 == "1"
+        then do
+          putStrLn (line ++ " Selected Pass")
+          let newRoller = (NB4G line Pass 0)
+          let newState = addNewRoller newRoller state
+          playNeedBeforeGreed newState
+      else if line2 == "2"
+        then do
+          putStrLn (line ++ " Selected Greed")
+          let newRoller = (NB4G line Greed 0)
+          let newState = addNewRoller newRoller state
+          playNeedBeforeGreed newState
+      else if line2 == "3"
+        then do
+          putStrLn (line ++ " Selected Need")
+          let newRoller = (NB4G line Need 0)
+          let newState = addNewRoller newRoller state
+          playNeedBeforeGreed newState
+      else do
+        -- On Rolltype selection the function asks the player to try again
+        -- if their input was not valid
+        putStrLn "Could not understand, please try again"
+        playNeedBeforeGreed state
 
-  -- Takes a list of NB4G greeds and returns the last element as the winner
-  -- assumes the list has already gone through rollNB4G
-  winnerNB4G :: [NB4G]->NB4G
-  winnerNB4G listofrolls =
-    last listofrolls
-    -- winnerNB4G (rollNB4G [(NB4G "Pass1" Pass 0), (NB4G "Greed1" Greed 0), (NB4G "Greed2" Greed 0), (NB4G "Need1" Need 0), (NB4G "Need2" Need 0), (NB4G "Greed3" Greed 0)])
 
-  -- rolls a 1d100 and puts it as the result of a NB4G that has the same name and type as the given NB4G
-  roll1d100 :: NB4G->NB4G
-  roll1d100 roller =
-    NB4G (name roller) (rolltype roller) (total (rollxdy 1 100))
-
-  -- returns false if the rolltype of roller isnt Pass
-  filterPass :: NB4G -> Bool
-  filterPass roller =
-    (rolltype roller) /= Pass
-
-  -- returns true if rolltype is Greed
-  takeGreed :: NB4G->Bool
-  takeGreed roller =
-    (rolltype roller) == Greed
-
-  -- returns true if rolltype is Need
-  takeNeed :: NB4G->Bool
-  takeNeed roller =
-    (rolltype roller) == Need
+-- Calls the rollNB4G function and handles the empty list case
+rollResult :: [NB4G]-> [NB4G]
+rollResult [] = []
+rollResult rollers = rollNB4G (rollers)
