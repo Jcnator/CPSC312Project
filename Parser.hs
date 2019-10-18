391cbab3edb9b0f908f6da54d470ba55714649f3
Jcnator <56492289+Jcnator@users.noreply.github.com>
2019-10-17 18:10:03 -0700

    Update Parser.hs

--- Parser.hs
+++ Parser.hs
@@ -13,7 +13,25 @@ module Parser where
       then do
         putStrLn "Starting Need Before Greed"
         playNeedBeforeGreed []
-        --putStrLn "Need Before Greed Ended"
+      else if
+        key_val == '?'
+        then do
+          putStrLn "Roll commands:"
+          putStrLn "Roll an X number of dice of Y sides with with the format:"
+          putStrLn "  XdY Example: 1d20, 2d100, 3d6"
+          putStrLn "Keep the Kst highst or lowest die with:"
+          putStrLn "  Example: 3d20kh1 returns the highest die of 3 Twenty Sided Dice"
+          putStrLn "  Example: 3d20kl1 returns the lowest die of 3 Twenty Sided Dice"
+          putStrLn "  Example: 3d20kh2 returns the second highest roll of 3 Twenty Sided Dice"
+          putStrLn "  Example: 3d20kl2 returns the second lowest roll of 3 Twenty Sided Dice"
+          putStrLn "Keep the the K higher or lower dice:"
+          putStrLn "  Example: 4d6kH3 returns the highest 3 dice of the four rolls"
+          putStrLn "  Example: 4d6kL3 returns the lowest 3 dice of the four rolls"
+          putStrLn "  Example: 4d6kL3 returns the lowest 3 dice of the four rolls"
+          putStrLn ""
+          putStrLn "Play Need Before Greed with the '$' command"
+
+
       else do
         let lists_d = makeLists (separator_d) input
         let lists_k = makeLists (separator_k) input
