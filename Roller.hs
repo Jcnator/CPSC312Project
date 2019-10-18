23cf1e98f256d5e4fa101313e4b6f8701ea2c444
Jcnator <56492289+Jcnator@users.noreply.github.com>
2019-10-17 17:16:35 -0700

    Update Roller.hs

--- Roller.hs
+++ Roller.hs
@@ -4,50 +4,42 @@ import System.Random
 import Data.List
 import System.IO.Unsafe
 
--- Roll is made of a final "result" which is the sum of the individual rolls in [Int]
-data Roll  = Roll Int [Int] deriving (Show)
+-- Roll is made of a final "result" which is the sum of
+-- the individual rolls. Roll also has a discarted field that holds
+-- any of the dice rolled that were not used for the total
+data Roll  = Roll {total :: Int
+                  , rolls :: [Int]
+                  , discarted :: [Int]
+                  } deriving (Show)
 
--- Creates a Roll data type
-makeRoll :: [Int] -> Roll
-makeRoll list =
-  Roll (listSum list) list
+-- Creates a Roll data type, the first member of the input is used rolls
+-- The second member of the input tuple is the rolls that are to be discarted
+-- input is a tuple to pass the results of the roller functions directly to
+-- make roll since it has a random element in it
+makeRoll :: ([Int],[Int]) -> Roll
+makeRoll input  =
+  Roll (listSum (fst input)) (fst input) (snd input)
 
 -- Basic roll: rolls x number of y sided dice
 rollxdy :: Int->Int->Roll
-rollxdy x y =
-  makeRoll (listRoller x y [])
+rollxdy x y = makeRoll ((listRoller x y []), [])
 
 -- Keep kst high: rolls x number of y sided dice and keep kst high
-rollxdykH :: Int->Int->Int->Roll
-rollxdykH x y k =
-  --let list = listRoller x y []
-  --listKstH (listRoller x y [])  k
-  makeRoll [listKstH (listRoller x y [])  k]
+rollxdykh :: Int->Int->Int->Roll
+rollxdykh x y k =  makeRoll (listKstH (listRoller x y []) k)
 
 -- Keep kst low: rolls x number of y sided dice and keep kst low
-rollxdykL:: Int->Int->Int->Roll
-rollxdykL x y k =
-  makeRoll [listKstL (listRoller x y [])  k]
+rollxdykl:: Int->Int->Int->Roll
+rollxdykl x y k = makeRoll (listKstL (listRoller x y []) k)
 
--- Keep k number of highest dice
-rollxdykHst :: Int->Int->Int->Roll
-rollxdykHst x y k =
-  makeRoll (listKeepHklist (listRoller x y [])  k)
-
+--Keep k number of highest dice
+rollxdykH :: Int->Int->Int->Roll
+rollxdykH x y k = makeRoll (listKeepHklist (listRoller x y [])  k)
+--
 -- Keep k number of lowest dice
-rollxdylHst :: Int->Int->Int->Roll
-rollxdylHst x y k =
-    makeRoll (listKeepHklist (listRoller x y [])  k)
+rollxdykL :: Int->Int->Int->Roll
+rollxdykL x y k = makeRoll (listKeepLklist (listRoller x y [])  k)
 
-
--- listRoller :: Int -> Int ->[Int] -> IO [Int]
--- listRoller a b list =
---   if a == 0 || b == 0
---     then
---       return  list
---     else do
---       n <- getStdRandom (randomR (1,b))
---       listRoller (a-1) b (n : list)
 
 -- Rolls a list of dice result in the adb form
 listRoller :: Int -> Int ->[Int] -> [Int]
@@ -59,39 +51,31 @@ listRoller a b list =
       let n = unsafePerformIO (getStdRandom (randomR (1,b)))
       listRoller (a-1) b (n : list)
 
--- sums the contents of a list
+-- Sums the contents of a list of integers
 listSum :: [Int]->Int
-listSum list =
-  foldr (+)  0 list
+listSum list = foldr (+)  0 list
 
--- returns the kst high value of a list
-listKstH :: [Int]-> Int ->Int
+-- Returns the kst high value of a list as the first member of the tuple, and the
+-- all the other values as the second member
+listKstH :: [Int]-> Int ->([Int],[Int])
 listKstH list  k =
-  (reverse (sort list)) !! (k-1)
+  ([(reverse (sort list)) !! (k-1)],
+  (take (k-1) (reverse (sort list))) ++ (drop (k)  (reverse (sort list))))
 
--- returns the kst low value of a list
-listKstL :: [Int]-> Int ->Int
+-- Returns the kst low value of a list as the first member of the tuple, and the
+-- all the other values as the second member
+listKstL :: [Int]-> Int ->([Int],[Int])
 listKstL list  k =
-  (sort list) !! (k-1)
+  ([(sort list) !! (k-1)],
+  (take (k-1) (sort list)) ++ (drop (k)   (sort list)))
 
--- keeps only the highest k values of a list
-listKeepHklist :: [Int] -> Int -> [Int]
-listKeepHklist list k =
-  fst (splitAt k (reverse (sort list)))
+-- Returns the highest k values of a list as the first member of the tuple, and the
+-- all the other values as the second member
+listKeepHklist :: [Int] -> Int -> ([Int],[Int])
+listKeepHklist list k = (splitAt k (reverse (sort list)))
 
--- keeps only the lowest k values of a list
-listKeepLklist :: [Int] -> Int -> [Int]
-listKeepLklist list k =
-    fst (splitAt k (sort list))
+-- Returns the highest k values of a list as the first member of the tuple, and the
+-- all the other values as the second member
+listKeepLklist :: [Int] -> Int -> ([Int],[Int])
+listKeepLklist list k = (splitAt k (sort list))
 
--- roller :: Int -> Int -> Int ->  IO Int
--- roller a b total  =
---    if a == 0 || b == 0
---      then do
---        --putStrLn("Total: " ++ total)
---        return total
---      else do
---       n <- getStdRandom (randomR (1,b))
---       -- n : (Dice roll)
---       print n
---       roller (a-1) b (n + total)
