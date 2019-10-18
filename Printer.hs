dac06e01a987d24e14737ec6cd7a4f70f2a8336e
Jcnator <56492289+Jcnator@users.noreply.github.com>
2019-10-17 17:32:25 -0700

    Update Printer.hs

--- Printer.hs
+++ Printer.hs
@@ -8,23 +8,21 @@ module Printer where
   import System.IO.Unsafe
 
 
+  -- Prints the contents of a Roll
   printRoll :: Roll->IO ()
   printRoll roll = do
     let tot = total roll
     let dice = rolls roll
-    putStrLn("Rolled a " ++ show tot ++ "from " ++ show dice)
+    let discarted_dice = discarted roll
+    if discarted_dice == []
+      then putStrLn("Rolled: " ++ show tot ++ " from " ++ show dice)
+      else putStrLn("Rolled: " ++ show tot ++ " from " ++ show dice ++ " discarting " ++ show discarted_dice)
 
+  -- Prints the contents of a NB4G
   printNB4G :: NB4G -> IO ()
   printNB4G player = do
     let n = name player
     let rt = rolltype player
     let r = result player
     putStrLn(show rt ++ " Roll- " ++ show r ++ " by " ++  n)
-    return player
-
-  -- printNB4GIO :: NB4G -> IO ()
-  -- printNB4GIO player = do
-  --   let n = name player
-  --   let rt = rolltype player
-  --   let r = result player
-  --   putStrLn(show rt ++ " Roll- " ++ show r ++ " by " ++ show n)
+  
